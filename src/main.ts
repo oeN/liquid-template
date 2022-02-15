1d7960521ea64c61e9f8c520551f22dd891557e6
Diomede T <diomede.tripicchio@gmail.com>
2022-02-15 23:07:25 +0100

    Fix the problem with the Live Preview editor

--- src/main.ts
+++ src/main.ts
@@ -5,40 +5,21 @@ import { LiquidTemplatesSettings, DEFAULT_SETTINGS, LiquidTemplatesSettingsTab }
 import TemplateSuggest from "./suggest/template-suggest";
 
 export default class LiquidTemplates extends Plugin {
-  private autosuggest: TemplateSuggest;
   public settings: LiquidTemplatesSettings;
 
   async onload(): Promise<void> {
     console.log('loading liquid templates plugin');
     await this.loadSettings();
 
-    this.setupAutosuggest();
+    this.addSettingTab(new LiquidTemplatesSettingsTab(this.app, this));
 
-    this.addSettingTab(new LiquidTemplatesSettingsTab(this.app, this))
+    this.registerEditorSuggest(new TemplateSuggest(this.app, this));
   }
 
   onunload(): void {
     console.log('unloading liquid templates plugin');
-    // remove the autosuggest handler when unloading the plugin
-    this.app.workspace.iterateCodeMirrors((cm: CodeMirror.Editor) => {
-      cm.off("change", this.autosuggestHandler);
-    })
   }
 
-  setupAutosuggest(): void {
-		this.autosuggest = new TemplateSuggest(this.app, this);
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			cm.on("change", this.autosuggestHandler);
-		});
-  }
-
-  autosuggestHandler = (
-    cmEditor: CodeMirror.Editor,
-    changeObj: CodeMirror.EditorChange
-  ): boolean => {
-    return this.autosuggest?.update(cmEditor, changeObj);
-  };
 
   async loadSettings(): Promise<void> {
     this.settings = this.settingsWithDefault(await this.loadData());
@@ -46,7 +27,7 @@ export default class LiquidTemplates extends Plugin {
 
   async saveSettings(): Promise<void> {
     // the templates folder can change
-    this.setupAutosuggest();
+    // this.setupAutosuggest();
     await this.saveData(this.settings);
   }
 
